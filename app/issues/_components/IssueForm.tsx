aed62bb6ab7ac237fb4939f0a6f5553b4440a260
Jeeva <jeevat13@gmail.com>
2025-01-13 23:24:04 +0530

    Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { Button, TextField, TextArea, Callout, Text } from "@radix-ui/themes";
-import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
@@ -14,12 +13,9 @@ import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { Issue } from "@prisma/client";
+import SimpleMDE from "react-simplemde-editor";
 
 type IssueFormData = z.infer<typeof issueSchema>;
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const {
